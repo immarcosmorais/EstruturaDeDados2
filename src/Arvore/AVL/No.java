2b6cbadaddda007c65daebfbaa135ece2f12dc20
marcos <marcos@marcos-PC>
2018-09-13 21:38:00 -0300

    Inicial...

---
+++ src/Arvore/AVL/No.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arvore.AVL;
+
+/**
+ *
+ * @author marcos
+ */
+class No {
+
+    public long item;
+    public No dir;
+    public No esq;
+    public int fatBal;
+    
+}
---
+++ src/Arvore/Binaria/No.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arvore.Binaria;
+
+/**
+ *
+ * @author marcos
+ */
+class No {
+
+    public long item;
+    public No dir;
+    public No esq;
+    
+}
